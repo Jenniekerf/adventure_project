78ff3674055e06a607ea6e061d859a9164987aec
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-28 17:37:53 +0000

    added list_option method

--- lib/adventure_project/cli.rb
+++ lib/adventure_project/cli.rb
@@ -1,8 +1,17 @@
 class AdventureProject::CLI 
   
  def call
-   puts "Welcome! Are you ready for an adventure? Tell me how adventurous you are!"
+   puts "Welcome! Are you ready for an adventure?"
+   #need a y/n option + input
+   list_options
  end
+ 
+ 
+ 
+ def list_options
+   puts "Tell me how adventurous you are! 1. I like cool sights but I tend to get scared easily. 2. I love adventures but I have bad knees so I can't do anything too crazy. 3. Bring it on! I'm a crazy adrenaline junkie!"
+ end
+ 
   
   
 end
