4ba9cf257541d447cc0d533b411e139a57e4a35a
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-05 23:10:02 +0000

    scraped all descriptions

--- lib/adventure_project/trips.rb
+++ lib/adventure_project/trips.rb
@@ -16,13 +16,22 @@ class AdventureProject::Trips
 
   def self.scraped_from_site
     doc = Nokogiri::HTML(open("https://www.wanderlust.co.uk/content/11-of-the-best-trips-for-adrenaline-junkies/"))
-    binding.pry
+    description = []
+    
     title = doc.css("div.articleBodyContent h3").text
+    description1 = doc.css("div.articleBodyContent").text.split("  ")[1].split(".").drop(1).join
+    description2 = doc.css("div.articleBodyContent").text.split("  ")[11]
+    description3 = doc.css("div.articleBodyContent").text.split("  ")[21]
+    description4 = doc.css("div.articleBodyContent").text.split("  ")[31]
+    description5 = doc.css("div.articleBodyContent").text.split("  ")[41]
+    description6 = doc.css("div.articleBodyContent").text.split("  ")[52]
+    description7 = doc.css("div.articleBodyContent").text.split("  ")[62]
+    description8 = doc.css("div.articleBodyContent").text.split("  ")[70]
+    description9 = doc.css("div.articleBodyContent").text.split("  ")[79]
+    description10 = doc.css("div.articleBodyContent").text.split("  ")[90]
+    description11 = doc.css("div.articleBodyContent").text.split("  ")[99]
   end
 
 
 
   end
-
-
-description1 = doc.css("div.articleBodyContent").text.split("  ")[1].split(".").drop(1).join 
