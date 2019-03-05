8b9d31b3c43456af47fd95e0213077c06277ae56
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-05 21:16:43 +0000

    scraped first description

--- lib/adventure_project/trips.rb
+++ lib/adventure_project/trips.rb
@@ -1,30 +1,28 @@
 class AdventureProject::Trips
-  
+
   attr_accessor :title, :description, :url
 
 
 
- def self.all 
+ def self.all
    self.scraped_trips
    end
-  
+
   def self.scraped_trips
     trips = []
     trips << self.scraped_from_site
-    trips 
+    trips
   end
-  
+
   def self.scraped_from_site
     doc = Nokogiri::HTML(open("https://www.wanderlust.co.uk/content/11-of-the-best-trips-for-adrenaline-junkies/"))
-    title = doc.css("div.articleBodyContent h3").first.text
+    binding.pry
+    title = doc.css("div.articleBodyContent h3").text
   end
-    
-    
+
+
+
   end
-  
 
 
-
-#stuff i scraped:
-#title trip 1 : <span class="o-PhotoGalleryPromo__a-HeadlineText">Hike in Torres del Paine, Chilean Patagonia</span>
-#description trip 1 : <div class="o-PhotoGalleryPromo__a-Description asset-description" 
+description1 = doc.css("div.articleBodyContent").text.split("  ")[1].split(".").drop(1).join 
