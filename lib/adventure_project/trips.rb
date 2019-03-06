af3881e1b967a1155b433780c19ef920224c6c77
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-06 17:19:23 +0000

    changed url

--- lib/adventure_project/trips.rb
+++ lib/adventure_project/trips.rb
@@ -16,16 +16,8 @@ class AdventureProject::Trips
 
   def self.scraped_from_site
     doc = Nokogiri::HTML(open("https://www.wanderlust.co.uk/content/11-of-the-best-trips-for-adrenaline-junkies/"))
-    title = doc.css("div.articleBodyContent h3").text
-    binding.pry
-    url = []
-    url << doc.css("div.articleBodyContent strong a").attr("href").value
-
-  end
-
-  def description
     description = []
-    description << doc.css("div.articleBodyContent").text.split("  ")[1].split(".").drop(1).join
+    description << doc.css("div.articleBodyContent").text.split("  ")[1]
     description << doc.css("div.articleBodyContent").text.split("  ")[11]
     description << doc.css("div.articleBodyContent").text.split("  ")[21]
     description << doc.css("div.articleBodyContent").text.split("  ")[31]
@@ -36,6 +28,10 @@ class AdventureProject::Trips
     description << doc.css("div.articleBodyContent").text.split("  ")[79]
     description << doc.css("div.articleBodyContent").text.split("  ")[90]
     description << doc.css("div.articleBodyContent").text.split("  ")[99]
+    title = doc.css("div.articleBodyContent h3").text.gsub!(/\d+/,"").split(".")
+    url = []
+    url << doc.css("div.articleBodyContent strong a").attr("href").value
+    binding.pry
   end
 
 
